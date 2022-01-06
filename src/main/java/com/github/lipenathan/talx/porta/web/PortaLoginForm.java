32d9eb9b7567096d1df5519d8ee42bcf950581cb
Felipe Nathan <felipenatthan@gmail.com>
2022-01-05 22:37:16 -0300

    Autorizador e redirecionador de usuário; Tela de conversas,

--- src/main/java/com/github/lipenathan/talx/porta/web/PortaLoginForm.java
+++ src/main/java/com/github/lipenathan/talx/porta/web/PortaLoginForm.java
@@ -4,6 +4,7 @@ import com.github.lipenathan.talx.core.dominio.Usuario;
 import com.github.lipenathan.talx.core.processos.ProcessoLogin;
 import com.github.lipenathan.talx.core.servicos.repositorio.imp.fabrica.FabricaRepositorioUsuario;
 import com.github.lipenathan.talx.infra.exception.NegocioException;
+import com.github.lipenathan.talx.infra.web.RedirectView;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -18,22 +19,24 @@ public class PortaLoginForm implements Serializable {
     private static final Long serialVersionUID = 1L;
     private String login;
     private String senha;
-    private ProcessoLogin processoLogin;
+    private final ProcessoLogin processoLogin;
 
     public PortaLoginForm() {
         this.processoLogin = new ProcessoLogin(FabricaRepositorioUsuario.getRepositorioUsuario());
     }
 
-    public void logar() {
+    public RedirectView logar() {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         System.out.println("Realizando login com usuário - " + login);
         try {
             Usuario usuario = processoLogin.logar(login, senha);
             facesContext.getExternalContext().getSessionMap().put("usuarioLogado", usuario);
+            return new RedirectView("conversas");
         } catch (NegocioException e) {
             facesContext.getExternalContext().getFlash().setKeepMessages(true);
             facesContext.addMessage(null, new FacesMessage("Usuário não encontrado"));
         }
+        return null;
     }
 
     public String getLogin() {
